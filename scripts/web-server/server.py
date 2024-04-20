7739f51fd40e961a79fa296ca396c927ef1951db
jaimegallegoo <jaimegallegochillon@gmail.com>
2024-04-20 12:06:02 +0200

    Redirigir a la página de la ciudad después de convertir los datos

--- scripts/web-server/server.py
+++ scripts/web-server/server.py
@@ -1,16 +1,22 @@
 from flask import Flask, send_from_directory, request, redirect, url_for, jsonify
+from flask import render_template
 import sys
 sys.path.append('/data-conversion/sumo-to-fiware')
 sys.path.append('/data-conversion/fiware-to-sumo')
 from functions_fiware_to_sumo import *
 from functions_sumo_to_fiware import *
 
-app = Flask(__name__, static_folder='../web')
+app = Flask(__name__, static_folder='../web', template_folder='../web/templates')
 
 # Serve the index.html file
 @app.route('/')
 def home():
     return send_from_directory(app.static_folder, 'index.html')
+
+# Serve any CSS file
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory(app.static_folder + '/css', path)
 
 # Serve the SUMO-conversion.html file
 @app.route('/SUMO-conversion', methods=['GET', 'POST'])
@@ -29,14 +35,9 @@ def conversion():
     return send_from_directory(app.static_folder, 'SUMO-conversion.html')
 
 # Serve the city.html file
-@app.route('/city')
-def city():
-    return send_from_directory(app.static_folder, 'city.html')
-
-# Serve any CSS file
-@app.route('/css/<path:path>')
-def send_css(path):
-    return send_from_directory(app.static_folder + '/css', path)
+@app.route('/cities/<city>')
+def city(city):
+    return render_template('city.html', city=city)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
